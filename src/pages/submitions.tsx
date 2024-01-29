0dbf7662fd70a465d5710dcbd9f149936b9ad1c3
Jorge <jorgesa.work@gmail.com>
2024-01-29 22:08:34 +0000

    added table to display values on DB

--- src/pages/submitions.tsx
+++ src/pages/submitions.tsx
@@ -1,8 +1,11 @@
 import Head from "next/head";
+import { DataTable } from "~/components/DataTable";
+import { columns } from "~/components/ui/columns";
 import { api } from "~/utils/api";
 
 export default function Submitions() {
   const submitions = api.getSubmitions.getSubmtions.useQuery();
+
   return (
     <>
       <Head>
@@ -10,7 +13,12 @@ export default function Submitions() {
         <meta name="description" content="All form submitions by the user" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="container mx-auto mt-24 flex min-h-screen flex-col gap-4 px-8"></main>
+      <main className="container mx-auto mt-24 flex min-h-screen flex-col gap-4 px-8">
+        <DataTable
+          data={submitions.data ? submitions.data : []}
+          columns={columns}
+        />
+      </main>
     </>
   );
 }
